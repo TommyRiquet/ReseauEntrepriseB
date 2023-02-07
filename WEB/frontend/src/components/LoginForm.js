18ec901aa2b6d524ec97c6600e7ee79e0d4d251d
louislombaert <71373079+LouisLombaert@users.noreply.github.com>
2023-02-07 16:52:31 +0100

    changes: signin/login forms

--- WEB/frontend/src/components/LoginForm.js
+++ WEB/frontend/src/components/LoginForm.js
@@ -1,22 +1,33 @@
-import {Button,Form} from 'react-bootstrap';
+import {Button,Form, Row, Col} from 'react-bootstrap';
 
 function LoginForm() {
   return (
     <>
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Pseudo</Form.Label>
-          <Form.Control type="text" placeholder="Entrer votre pseudo" />
-        </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Pseudo
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control required type="text" placeholder="pseudo" />
+            </Col>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Mot de passe</Form.Label>
-          <Form.Control type="password" placeholder="Mot de passe" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Soumettre
-        </Button>
-      </Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+            <Form.Label column sm={2}>
+              Mot de passe
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control required type="password" placeholder="Mot de passe" />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">Se connecter</Button>
+            </Col>
+          </Form.Group>
+        </Form>
     </>
   );
 }
--- WEB/frontend/src/components/SigninForm.js
+++ WEB/frontend/src/components/SigninForm.js
@@ -1,34 +1,93 @@
-import {Button,Form} from 'react-bootstrap';
+import React, { useState } from 'react';
+import {Button,Form, Row, Col} from 'react-bootstrap';
+//import * as Yup from 'yup';
 
 function SigninForm() {
-    return (
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    //pour validation du mot de passe 
+    // !!! VALIDATION PAS ENCORE FONCTIONELLE !!!
+    /*validationSchema: Yup.object({
+      password: Yup.string().required('Password is required'),
+      passwordConfirmation: Yup.string()
+         .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    });*/ 
+
+    setValidated(true);
+  };
+  
+  return (
       <>
         <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Adresse mail</Form.Label>
-          <Form.Control type="email" placeholder="Entrer votre addresse" />
-        </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Adresse mail
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control required type="email" placeholder="Email" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Pseudo
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control required type="text" placeholder="pseudo" />
+            </Col>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Pseudo</Form.Label>
-          <Form.Control type="text" placeholder="Entrer votre pseudo" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Mot de passe</Form.Label>
-          <Form.Control type="password" placeholder="Mot de passe" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Selectionner votre rôle</Form.Label>
-          <Form.Select aria-label="Select role">
-            <option>Selectionner</option>
-            <option value="student">Etudiant</option>
-            <option value="former">Formateur</option>
-          </Form.Select>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Soumettre 
-        </Button>
-      </Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+            <Form.Label column sm={2}>
+              Mot de passe
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control required type="password" placeholder="Mot de passe" />
+            </Col>
+          </Form.Group>
+          <Form.group as={Row} className="mb-3" controlId="formHorizontalPasswordVerif"></Form.group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalConfirmPassword">
+            <Form.Label column sm={2}>
+             Confirmez le Mot de passe
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control required type="password" placeholder="Mot de passe" />
+            </Col>
+          </Form.Group>
+          <fieldset>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label as="legend" column sm={2}>
+                Rôle
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Check required
+                  type="radio"
+                  label="étudiant"
+                  name="formHorizontalRadios"
+                  id="student"
+                />
+                <Form.Check required
+                  type="radio"
+                  label="formateur"
+                  name="formHorizontalRadios"
+                  id="former"
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
+          
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">S'inscrire</Button>
+            </Col>
+          </Form.Group>
+        </Form>
       </>
     );
   }
