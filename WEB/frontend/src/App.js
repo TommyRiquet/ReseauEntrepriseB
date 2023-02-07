c3d1c257c0546f08168dc68102a7a4d890392433
TommyRiquet <tommy.riquet@hotmail.com>
2023-02-07 10:56:21 +0100

    feat(Library): Page Library
    Ajout de la page library afin d'afficher les formations correspondantes.

--- WEB/frontend/src/App.js
+++ WEB/frontend/src/App.js
@@ -2,20 +2,20 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Accueil from "./pages/Accueil";
 import Error from "./pages/Error";
-
-import "./App.css";
+import Library from './pages/Library';
 import Inscription from './pages/Inscription';
 
+import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter className='center-container'>
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/inscription/:login" element={<Inscription />} />
-        <Route path="/bibli" element={<Accueil />} />
+        <Route path="/bibli" element={<Library />} />
 
         {/* Lorsqu'aucune route n'a été trouvé */}
         <Route path="*" element={<Error />} />
