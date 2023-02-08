ebc8ee04999bc964d609ed3bfa09476b3678d206
TommyRiquet <tommy.riquet@hotmail.com>
2023-02-08 13:29:33 +0100

    feat(form): inscription/connection

--- WEB/frontend/src/components/LoginForm.js
+++ WEB/frontend/src/components/LoginForm.js
@@ -1,33 +1,38 @@
-import {Button,Form, Row, Col} from 'react-bootstrap';
+import {Button,Form, Row, Col,Container} from 'react-bootstrap';
 
 function LoginForm() {
   return (
     <>
+      <Container className='center-container'>
+      <div className='form-layout'>
+      <h1>Connection</h1>
       <Form>
           <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-            <Form.Label column sm={2}>
+            <Form.Label>
               Pseudo
             </Form.Label>
-            <Col sm={5}>
-              <Form.Control required type="text" placeholder="pseudo" />
+            <Col>
+              <Form.Control required type="text" placeholder="Pseudo" />
             </Col>
           </Form.Group>
 
           <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-            <Form.Label column sm={2}>
+            <Form.Label>
               Mot de passe
             </Form.Label>
-            <Col sm={5}>
+            <Col>
               <Form.Control required type="password" placeholder="Mot de passe" />
             </Col>
           </Form.Group>
 
           <Form.Group as={Row} className="mb-3">
-            <Col sm={{ span: 10, offset: 2 }}>
-              <Button type="submit" onClick={e=>window.location.href='/connexionAppel'}>Se connecter</Button>
+            <Col className='center-div'>
+              <Button type="submit"  variant="danger" onClick={e=>window.location.href='/connexionAppel'}>Se connecter</Button>
             </Col>
           </Form.Group>
         </Form>
+        </div>
+        </Container>
     </>
   );
 }
