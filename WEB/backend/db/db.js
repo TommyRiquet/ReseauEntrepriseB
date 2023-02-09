f4801b7c6a4cea509772c51a205a1d7f4a4485f8
TommyRiquet <tommy.riquet@hotmail.com>
2023-02-09 14:55:52 +0100

    feat: Database

--- WEB/backend/db/db.js
+++ WEB/backend/db/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "riquet2961",
+  password: "",
   host: "localhost",
   port: 5432,
   database: "postgres"
